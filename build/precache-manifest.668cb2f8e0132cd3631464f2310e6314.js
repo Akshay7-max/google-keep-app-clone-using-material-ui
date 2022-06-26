3c54ae37b5ffabf0d7b1e7c4f494fdb601de4827
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-06-27 00:47:12 +0530

    initial commit

---
+++ build/precache-manifest.668cb2f8e0132cd3631464f2310e6314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ceeb58b4d09fb4aeb3b",
+    "url": "/google-keep-app-clone-using-material-ui/static/css/main.82a76c8e.chunk.css"
+  },
+  {
+    "revision": "9ceeb58b4d09fb4aeb3b",
+    "url": "/google-keep-app-clone-using-material-ui/static/js/main.9ceeb58b.chunk.js"
+  },
+  {
+    "revision": "445173022f3168a2383e",
+    "url": "/google-keep-app-clone-using-material-ui/static/js/1.44517302.chunk.js"
+  },
+  {
+    "revision": "b00a20349996b30b5450",
+    "url": "/google-keep-app-clone-using-material-ui/static/js/runtime~main.b00a2034.js"
+  },
+  {
+    "revision": "aa2eba3cdd103f212122ab1d1efef028",
+    "url": "/google-keep-app-clone-using-material-ui/index.html"
+  }
+];
